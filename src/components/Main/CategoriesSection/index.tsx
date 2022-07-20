c4f61e97e97d0991c5b538e49bd89b43c3955c9b
Eunbi Kang <jenabill@naver.com>
2022-07-20 17:44:53 +0900

    Refactor: CategoriesSection component

--- src/components/Main/CategoriesSection/index.tsx
+++ src/components/Main/CategoriesSection/index.tsx
@@ -1,28 +1,36 @@
-import CategoryItem from "./CategoryItem";
+import CategoryAddButton from "./CategoryAddButton";
 import styled from "styled-components";
+import CategoryList from "./CategoryList";
 
 const CategoriesSection = () => {
   return (
-    <section>
-      <Heading>
-        <h2>
-          <span>📚</span> Categories
-        </h2>
-      </Heading>
-      <div>
-        <CategoryItem categoryName="HTML" memoCnt={10} />
-        <CategoryItem categoryName="React" memoCnt={10} />
-      </div>
-    </section>
+    <>
+      <Section>
+        <Heading>
+          <h2>
+            <span>📚</span> Categories
+          </h2>
+        </Heading>
+        <CategoryList />
+      </Section>
+      <CategoryAddButton />
+    </>
   );
 };
 
 export default CategoriesSection;
 
+const Section = styled.section`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
 const Heading = styled.div`
   border-bottom: 1px solid #212121;
-  padding: 1rem;
+  padding: 1rem 0;
   h2 > span {
     font-size: 1.2rem;
+    margin-right: 3px;
   }
 `;
