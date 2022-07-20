2a83e3b85a4962004f2cc19738532e7a6ab4cd64
Eunbi Kang <jenabill@naver.com>
2022-07-20 23:01:47 +0900

    Refactor: CategoryItem component

--- src/components/Main/CategoriesSection/CategoryItem.tsx
+++ src/components/Main/CategoriesSection/CategoryItem.tsx
@@ -1,18 +1,42 @@
 import styled from "styled-components";
+import { Category } from "../../../types/category";
+import DropdownMenu from "../../common/DropdownMenu";
+import { useToggle } from "../../../hooks/useToggle";
+import { useNavigate } from "react-router-dom";
+import { BsLink45Deg } from "react-icons/bs";
 
 interface CategoryItemProps {
-  categoryName: string;
-  memoCnt: number;
+  category: Category;
+  clickEditMenu: () => void;
+  clickRemoveMenu: () => void;
 }
 
-const CategoryItem = ({ categoryName, memoCnt }: CategoryItemProps) => {
+const CategoryItem = ({
+  category,
+  clickEditMenu,
+  clickRemoveMenu,
+}: CategoryItemProps) => {
+  const { categoryId, categoryName, memoCnt } = category;
+  const [show, toggle, onClose] = useToggle(false);
+  const navigate = useNavigate();
   return (
-    <Item>
-      <div>
+    <Item show={show}>
+      <div
+        onClick={() => navigate(`/memo/${categoryId}`, { state: categoryName })}
+      >
         <h3>{categoryName}</h3>
       </div>
       <div>
-        <p>🔗️ {memoCnt}</p>
+        <p>
+          <BsLink45Deg /> {memoCnt}
+        </p>
+        <DropdownMenu
+          show={show}
+          toggle={toggle}
+          onClose={onClose}
+          clickEditMenu={clickEditMenu}
+          clickRemoveMenu={clickRemoveMenu}
+        />
       </div>
     </Item>
   );
@@ -20,11 +44,19 @@ const CategoryItem = ({ categoryName, memoCnt }: CategoryItemProps) => {
 
 export default CategoryItem;
 
-const Item = styled.div`
+const Item = styled.li<{ show: boolean }>`
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 1px solid lightgray;
   padding: 1.5rem 2rem;
   font-weight: normal;
+  z-index: ${({ show }) => show && "1"};
+  > div {
+    display: flex;
+    gap: 1rem;
+    &:first-child {
+      cursor: pointer;
+    }
+  }
 `;
