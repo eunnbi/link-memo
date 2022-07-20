c7353e8ab9086e14bad3f50b1aa663a232650a44
Eunbi Kang <jenabill@naver.com>
2022-07-20 22:51:58 +0900

    Refactor: 라우팅 추가 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
 import SearchPage from "./pages/SearchPage";
+import LinkMemoPage from "./pages/LinkMemoPage";
+import EditPage from "./pages/EditPage";
 
 const queryClient = new QueryClient();
 function App() {
@@ -15,6 +17,12 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/search" element={<SearchPage />} />
+          <Route path="/memo/*" element={<LinkMemoPage />}>
+            <Route path=":categoryId" element={<LinkMemoPage />} />
+          </Route>
+          <Route path="/edit/*" element={<EditPage />}>
+            <Route path=":memoId" element={<EditPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
