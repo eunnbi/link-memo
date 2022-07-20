7a9e5b4c5a0b1109bdebf5e3c5e50a56aceee601
Eunbi Kang <jenabill@naver.com>
2022-07-20 17:36:55 +0900

    Add: PlusButton component

---
+++ src/components/common/PlusButton/index.tsx
@@ -0,0 +1,35 @@
+import { ImPencil } from "react-icons/im";
+import { HiPlus } from "react-icons/hi";
+import styled from "styled-components";
+
+interface PlusButtonProps {
+  onClick: () => void;
+}
+
+const PlusButton = ({ onClick }: PlusButtonProps) => {
+  return (
+    <CircleButton onClick={onClick} type="button">
+      <HiPlus />
+    </CircleButton>
+  );
+};
+
+export default PlusButton;
+
+const CircleButton = styled.button`
+  background-color: #000;
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  z-index: 3;
+  svg {
+    font-size: 2rem;
+    color: #fff;
+  }
+`;
