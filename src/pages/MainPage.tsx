d002760a56ba2cb6c056a97bf93c5e538e42b8bd
Eunbi Kang <jenabill@naver.com>
2022-07-20 15:05:42 +0900

    Add: Main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import CategoriesSection from "../components/Main/CategoriesSection";
+import MainHeader from "../components/Main/MainHeader";
+
+const MainPage = () => {
+  return (
+    <main>
+      <MainHeader />
+      <CategoriesSection />
+    </main>
+  );
+};
+
+export default MainPage;
