d58f2cc87f2e29b36829e4be1ba067bc748abea9
Eunbi Kang <jenabill@naver.com>
2022-07-20 22:47:07 +0900

    Rename: move useFetchCategories hook to common hooks folder

--- src/hooks/useFetchCategories.ts
+++ src/hooks/useFetchCategories.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import { categoriesKey } from "../../../../constants/queryKey";
-import { getCategories } from "../../../../api/categories";
-import { CategoriesResponse } from "../../../../types/category";
+import { categoriesKey } from "../constants/queryKey";
+import { getCategories } from "../api/categories";
+import { CategoriesResponse } from "../types/category";
 
 export const useFetchCategories = () => {
   const { data, status } = useQuery<CategoriesResponse>(
