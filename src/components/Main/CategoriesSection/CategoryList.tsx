783230db0d17635a835a4a53ebef5c5a654f819e
Eunbi Kang <jenabill@naver.com>
2022-07-20 17:43:53 +0900

    Add: CategoryList component

---
+++ src/components/Main/CategoriesSection/CategoryList.tsx
@@ -0,0 +1,54 @@
+import { useFetchCategories } from "./hooks/useFetchCategories";
+import CategoryItem from "./CategoryItem";
+import styled from "styled-components";
+import { useState } from "react";
+import DeleteAlert from "./DeleteAlert";
+import EditAlert from "./EditAlert";
+
+const CategoryList = () => {
+  const { data, status } = useFetchCategories();
+  const [deleteCategoryId, setDeleteCategoryId] = useState(0);
+  const [editCategory, setEditCategory] = useState({
+    categoryId: 0,
+    categoryName: "",
+  });
+
+  return data?.categories.length === 0 ? (
+    <NoCategories>
+      아래 (+) 버튼을 눌러 링크 메모를 보관할 나만의 카테고리를 만들어 보세요!
+    </NoCategories>
+  ) : (
+    <>
+      <ul>
+        {data?.categories.map((category) => (
+          <CategoryItem
+            key={category.categoryId}
+            category={category}
+            clickEditMenu={() =>
+              setEditCategory({
+                categoryId: category.categoryId,
+                categoryName: category.categoryName,
+              })
+            }
+            clickRemoveMenu={() => setDeleteCategoryId(category.categoryId)}
+          />
+        ))}
+      </ul>
+      <DeleteAlert
+        categoryId={deleteCategoryId}
+        setCategoryId={setDeleteCategoryId}
+      />
+      <EditAlert category={editCategory} setCategory={setEditCategory} />
+    </>
+  );
+};
+
+export default CategoryList;
+
+const NoCategories = styled.p`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.6;
+`;
