2c7a8294b268c01df86a4aca1b7b5ad769ebb5d9
Eunbi Kang <jenabill@naver.com>
2022-07-20 22:43:24 +0900

    Add: LinkInputs component

---
+++ src/components/Edit/LinkInputs/index.tsx
@@ -0,0 +1,38 @@
+import FormInput from "../../common/FormInput";
+import { useLinkInputs } from "./useLinkInputs";
+import { useInputChange } from "../hooks/useInputChange";
+import styled from "styled-components";
+
+const LinkInputs = () => {
+  const { linkName, linkUrl } = useLinkInputs();
+  const onChange = useInputChange();
+  return (
+    <InputBox>
+      <FormInput
+        label="링크 이름"
+        name="linkName"
+        value={linkName}
+        placeholder="naver"
+        onChange={onChange}
+        password={false}
+      />
+      <FormInput
+        label="링크 주소 (URL)"
+        name="linkUrl"
+        value={linkUrl}
+        placeholder="www.naver.com"
+        onChange={onChange}
+        password={false}
+      />
+    </InputBox>
+  );
+};
+
+export default LinkInputs;
+
+export const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 3.5rem;
+`;
