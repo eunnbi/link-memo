7223d04133f092fb34603caff24a20baece5f2db
Eunbi Kang <jenabill@naver.com>
2022-07-20 15:02:48 +0900

    Add: RegisterPage

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,23 @@
+import RegisterForm from "../components/Register/RegisterForm";
+import styled from "styled-components";
+
+const Main = styled.main`
+  max-width: 450px;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  margin-bottom: 0;
+  h1 {
+    margin: 0 0 2rem;
+  }
+`;
+const RegisterPage = () => {
+  return (
+    <Main>
+      <h1>회원가입</h1>
+      <RegisterForm />
+    </Main>
+  );
+};
+
+export default RegisterPage;
