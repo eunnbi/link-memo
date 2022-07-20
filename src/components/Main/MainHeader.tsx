a16a6e66cee1becaf5744c33ef6f2e6986e29781
Eunbi Kang <jenabill@naver.com>
2022-07-20 15:03:39 +0900

    Add: MainHeader component

---
+++ src/components/Main/MainHeader.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { GoSearch } from "react-icons/go";
+import { IconLink } from "../common/styles/IconLink.styles";
+import Logout from "./Logout";
+
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1.5rem 0 2rem;
+`;
+
+const MainHeader = () => {
+  return (
+    <Header>
+      <Logout />
+      <h1>Link Memo</h1>
+      <IconLink to="/search">
+        <GoSearch />
+      </IconLink>
+    </Header>
+  );
+};
+
+export default MainHeader;
