1714846b370a975f8a55c2a9ccde23db748af01e
Eunbi Kang <jenabill@naver.com>
2022-07-22 01:57:08 +0900

    Refactor: useEdit 훅에 api 호출 관련 로직 추가

--- src/components/Edit/EditForm/useEdit.ts
+++ src/components/Edit/EditForm/useEdit.ts
@@ -4,11 +4,40 @@ import { useSelector } from "react-redux";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { RootState } from "../../../modules";
 import { SuccessResponse } from "../../../types";
-import { LinkMemoState } from "../../../types/linkMemo";
+import {
+  LinkMemoId,
+  LinkMemoRequest,
+  LinkMemoState,
+} from "../../../types/linkMemo";
+import { patchLinkMemo, postLinkMemo } from "../../../api/linkMemos";
 
 export const useEdit = (id: number | undefined) => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const onEditSuccess = (categoryId: number, categoryName: string) => {
+    queryClient.invalidateQueries();
+    navigate(`/memo/${categoryId}`, { state: { categoryName } });
+  };
+
+  const postMutation = useMutation<LinkMemoId, unknown, LinkMemoRequest>(
+    ({ linkName, linkUrl, content, categoryId }) =>
+      postLinkMemo(linkName, linkUrl, content, categoryId),
+    {
+      onSuccess: (data, { categoryId, categoryName }) => {
+        onEditSuccess(categoryId, categoryName!);
+      },
+    }
+  );
+
+  const patchMutation = useMutation<SuccessResponse, unknown, LinkMemoRequest>(
+    ({ linkName, linkUrl, content, categoryId, memoId }) =>
+      patchLinkMemo(linkName, linkUrl, content, categoryId, memoId!),
+    {
+      onSuccess: (data, { categoryId, categoryName }) => {
+        onEditSuccess(categoryId, categoryName!);
+      },
+    }
+  );
 
   const [warningText, setWarningText] = useState("");
   const { linkName, linkUrl, content, category }: LinkMemoState = useSelector(
@@ -24,6 +53,29 @@ export const useEdit = (id: number | undefined) => {
       setWarningText("URL 주소는 필수 입력 항목입니다.");
       return;
     }
+    if (id) {
+      patchMutation.mutate({
+        linkName,
+        linkUrl,
+        content,
+        categoryId: Number(category.categoryId),
+        categoryName: category.categoryName,
+        memoId: id,
+      });
+    } else {
+      postMutation.mutate({
+        linkName,
+        linkUrl,
+        content,
+        categoryId: Number(category.categoryId),
+        categoryName: category.categoryName,
+      });
+    }
   };
-  return { warningText, onEdit };
+  return {
+    warningText,
+    onEdit,
+    postLoading: postMutation.isLoading,
+    patchLoading: patchMutation.isLoading,
+  };
 };
