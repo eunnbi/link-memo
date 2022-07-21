e3d216567f79cef6409efd53cc9132cf075348d9
Eunbi Kang <jenabill@naver.com>
2022-07-22 02:00:11 +0900

    Refactor: likeMemo get api 관련 쿼리 키 수정

--- src/hooks/useFetchLinkMemos.ts
+++ src/hooks/useFetchLinkMemos.ts
@@ -3,12 +3,13 @@ import { getLinkMemos } from "../api/linkMemos";
 import { linkMemoKeys } from "../constants/queryKey";
 import { LinkMemosGetResponse } from "../types/linkMemo";
 
-export const useFetchLinkMemos = (
-  categoryId: number,
-  searchQuery: string | undefined = undefined
-) => {
+export const useFetchLinkMemos = (categoryId: number, searchQuery: string) => {
+  let queryKey = linkMemoKeys.byCategory(categoryId);
+  if (searchQuery !== "") {
+    queryKey = linkMemoKeys.bySearchQuery(categoryId, searchQuery);
+  }
   const { data, status } = useQuery<LinkMemosGetResponse>(
-    linkMemoKeys.byCategory(categoryId),
+    queryKey,
     () => getLinkMemos(categoryId, searchQuery),
     {
       refetchOnWindowFocus: false,
