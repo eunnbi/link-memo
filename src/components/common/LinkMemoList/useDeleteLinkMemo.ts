36b013eb4ebfe04e23ffc8bf1eb2aaebc1253dc4
Eunbi Kang <jenabill@naver.com>
2022-07-22 01:58:08 +0900

    Refactor: linkMemo delete api 호출 성공 이후 쿼리 무효화 로직 수정

--- src/components/common/LinkMemoList/useDeleteLinkMemo.ts
+++ src/components/common/LinkMemoList/useDeleteLinkMemo.ts
@@ -2,16 +2,14 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteLinkMemo } from "../../../api/linkMemos";
 import { SuccessResponse } from "../../../types";
 import { LinkMemoId } from "../../../types/linkMemo";
-import { categoriesKey } from "../../../constants/queryKey";
 
-export const useDeleteLinkMemo = (queryKey: any, onClose: () => void) => {
+export const useDeleteLinkMemo = (onClose: () => void) => {
   const queryClient = useQueryClient();
   const deleteMutation = useMutation<SuccessResponse, unknown, LinkMemoId>(
     ({ memoId }) => deleteLinkMemo(memoId),
     {
       onSuccess: () => {
-        queryClient.invalidateQueries(queryKey);
-        queryClient.invalidateQueries(categoriesKey);
+        queryClient.invalidateQueries();
         onClose();
       },
     }
