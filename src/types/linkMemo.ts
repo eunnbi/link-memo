f4fe4c1f799eca627a80d4de881c3efcaf22e93f
Eunbi Kang <jenabill@naver.com>
2022-07-21 22:24:07 +0900

    Refactor: linkMemo types

--- src/types/linkMemo.ts
+++ src/types/linkMemo.ts
@@ -7,12 +7,22 @@ export interface LinkMemoState {
   category: Category;
 }
 
+export interface LinkMemoId {
+  memoId: number;
+}
+
 export interface LinkMemoResponse {
-  linkMemoId: number;
+  memoId?: LinkMemoId["memoId"];
   linkName: LinkMemoState["linkName"];
   linkUrl: LinkMemoState["linkUrl"];
   content: LinkMemoState["content"];
   categoryId: Category["categoryId"];
-  categoryName: Category["categoryName"];
-  like: boolean;
+  categoryName?: Category["categoryName"];
+  like?: boolean;
 }
+
+export interface LinkMemosGetResponse {
+  linkMemos: LinkMemoResponse[];
+}
+
+export type LinkMemoRequest = LinkMemoResponse;
