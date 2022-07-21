e32609eadf5fc526c06a29522cab1280f29c37c6
Eunbi Kang <jenabill@naver.com>
2022-07-22 02:02:10 +0900

    Refactor: 카카오톡 api 사용하기 위해 key 초기화하는 로직 추가 및 Search page 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
-import SearchPage from "./pages/SearchPage";
 import LinkMemoPage from "./pages/LinkMemoPage";
 import EditPage from "./pages/EditPage";
 
 const queryClient = new QueryClient();
 function App() {
+  useEffect(() => {
+    console.log(process.env.REACT_APP_KAKAO_KEY);
+    window.Kakao.init(process.env.REACT_APP_KAKAO_KEY);
+    console.log(window.Kakao.isInitialized());
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
@@ -16,7 +21,6 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/main" element={<MainPage />} />
-          <Route path="/search" element={<SearchPage />} />
           <Route path="/memo/*" element={<LinkMemoPage />}>
             <Route path=":categoryId" element={<LinkMemoPage />} />
           </Route>
