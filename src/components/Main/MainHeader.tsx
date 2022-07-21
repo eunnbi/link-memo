0954d9561397052925e1371f5214086d4135014c
Eunbi Kang <jenabill@naver.com>
2022-07-22 02:47:30 +0900

    Refactor: MainHeader component

--- src/components/Main/MainHeader.tsx
+++ src/components/Main/MainHeader.tsx
@@ -1,30 +1,32 @@
 import styled from "styled-components";
-import { GoSearch } from "react-icons/go";
-import { IconLink } from "../common/styles/IconLink.styles";
 import Logout from "./Logout";
-
-const Header = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 1.5rem 0 2rem;
-  h1 {
-    font-family: Pangolin;
-    font-size: 2.5rem;
-  }
-`;
 
 const MainHeader = () => {
   return (
     <Header>
-      <Logout />
+      <div>
+        <Logout />
+      </div>
       <h1>Link Memo</h1>
-      <IconLink to="/search">
-        <GoSearch />
-      </IconLink>
     </Header>
   );
 };
 
 export default MainHeader;
+
+const Header = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 1.5rem 0 2rem;
+  & > div {
+    position: absolute;
+  }
+  h1 {
+    width: 100%;
+    font-family: Pangolin;
+    font-size: 2.5rem;
+    text-align: center;
+  }
+`;
