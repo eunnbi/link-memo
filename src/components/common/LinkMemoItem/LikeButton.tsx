d04c06373e94973d2465a586a7e2d3d76b665e3f
Eunbi Kang <jenabill@naver.com>
2022-07-22 01:57:39 +0900

    Refactor: LikeButton component에 좋아요 post api 호출 로직 추가

--- src/components/common/LinkMemoItem/LikeButton.tsx
+++ src/components/common/LinkMemoItem/LikeButton.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import { BsSuitHeartFill, BsSuitHeart } from "react-icons/bs";
 import styled from "styled-components";
+import { usePostLike } from "./usePostLike";
 
-const LikeButton = ({ initialLike }: { initialLike: boolean }) => {
+interface LikeButtonProps {
+  initialLike: boolean;
+  memoId: number;
+}
+
+const LikeButton = ({ initialLike, memoId }: LikeButtonProps) => {
   const [like, setLike] = useState(initialLike);
   const onLike = () => setLike(!like);
+
+  usePostLike(initialLike, like, memoId);
+
   return (
     <Button onClick={onLike}>
       {like ? <BsSuitHeartFill /> : <BsSuitHeart />}
