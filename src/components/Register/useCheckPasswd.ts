df0fdc08e769921f83eb5973c1953e0a2682b5a9
Eunbi Kang <jenabill@naver.com>
2022-07-22 22:31:33 +0900

    Refactor: 통일성을 위해 변수명을 passwd에서 password으로 수정

--- src/components/Register/useCheckPasswd.ts
+++ src/components/Register/useCheckPasswd.ts
@@ -1,25 +1,25 @@
-import { Dispatch, SetStateAction, useEffect } from 'react';
-import { IGuideText } from './RegisterForm';
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { IGuideText } from "./RegisterForm";
 
 export const useCheckPasswd = (
-  passwd: string,
+  password: string,
   checkPasswd: string,
-  setGuideText: Dispatch<SetStateAction<IGuideText>>,
+  setGuideText: Dispatch<SetStateAction<IGuideText>>
 ) => {
   useEffect(() => {
-    if (passwd === '' || checkPasswd === '') return;
-    if (passwd === checkPasswd) {
+    if (password === "" || checkPasswd === "") return;
+    if (password === checkPasswd) {
       setGuideText({
-        where: 'checkPasswd',
-        text: '비밀번호가 일치합니다!',
+        where: "checkPasswd",
+        text: "비밀번호가 일치합니다!",
         isWarning: false,
       });
     } else {
       setGuideText({
-        where: 'checkPasswd',
-        text: '비밀번호가 일치하지 않습니다.',
+        where: "checkPasswd",
+        text: "비밀번호가 일치하지 않습니다.",
         isWarning: true,
       });
     }
-  }, [checkPasswd, passwd]);
+  }, [checkPasswd, password]);
 };
