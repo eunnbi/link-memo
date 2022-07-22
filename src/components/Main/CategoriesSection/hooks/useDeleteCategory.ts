e6a07f2684c0b82e4482b9cc7cdda2ec74be7eac
Eunbi Kang <jenabill@naver.com>
2022-07-22 22:59:22 +0900

    Refactor: useDeleteCategory hook의 반환값 수정

--- src/components/Main/CategoriesSection/hooks/useDeleteCategory.ts
+++ src/components/Main/CategoriesSection/hooks/useDeleteCategory.ts
@@ -6,20 +6,13 @@ import { CategoryId } from "../../../../types/category";
 
 export const useDeleteCategory = (onCancel: () => void) => {
   const queryClient = useQueryClient();
-  const { mutate, isLoading } = useMutation<
-    SuccessResponse,
-    unknown,
-    CategoryId
-  >(({ categoryId }) => deleteCategory(categoryId), {
-    onSuccess: () => {
-      queryClient.invalidateQueries(categoriesKey);
-      onCancel();
-    },
-  });
-
-  const removeCategory = (categoryId: number) => {
-    mutate({ categoryId });
-  };
-
-  return { removeCategory, isLoading };
+  return useMutation<SuccessResponse, unknown, CategoryId>(
+    ({ categoryId }) => deleteCategory(categoryId),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(categoriesKey);
+        onCancel();
+      },
+    }
+  );
 };
