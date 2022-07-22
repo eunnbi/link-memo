fdd7e502873b0c593781425dfa057a03a84891e4
Eunbi Kang <jenabill@naver.com>
2022-07-22 23:07:57 +0900

    Refactor: DeleteAlert props 타입 수정

--- src/components/Main/CategoriesSection/DeleteAlert.tsx
+++ src/components/Main/CategoriesSection/DeleteAlert.tsx
@@ -2,22 +2,31 @@ import React, { useEffect } from "react";
 import { useDeleteCategory } from "./hooks/useDeleteCategory";
 import Alert from "../../common/Alert";
 import { useAlert } from "../../../hooks/useAlert";
+import { CategoryId } from "../../../types/category";
 
 interface DeleteAlertProps {
-  categoryId: number;
-  setCategoryId: React.Dispatch<React.SetStateAction<number>>;
+  category: CategoryId;
+  setCategory: React.Dispatch<CategoryId>;
 }
 
-const DeleteAlert = ({ categoryId, setCategoryId }: DeleteAlertProps) => {
+const DeleteAlert = ({ category, setCategory }: DeleteAlertProps) => {
+  const { categoryId } = category;
   const { show, onAlert, onClose } = useAlert();
 
   const onCancel = () => {
     onClose();
-    setCategoryId(0);
+    setCategory({
+      categoryId: 0,
+    });
   };
 
-  const { removeCategory, isLoading } = useDeleteCategory(onCancel);
+  const { mutate, isLoading } = useDeleteCategory(onCancel);
 
+  const onConfirm = () => {
+    mutate({ categoryId });
+  };
+
+  // alert
   useEffect(() => {
     if (categoryId === 0) return;
     onAlert();
@@ -29,7 +38,7 @@ const DeleteAlert = ({ categoryId, setCategoryId }: DeleteAlertProps) => {
       title={isLoading ? "카테고리 삭제 중..." : "카테고리 삭제"}
       message="해당 카테고리에 포함된 모든 메모들도 함께 삭제됩니다."
       onCancel={onCancel}
-      onConfirm={() => removeCategory(categoryId)}
+      onConfirm={onConfirm}
     />
   );
 };
