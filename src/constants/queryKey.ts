4addce2c45e68726052467f4bef5af38a7a9adf0
Eunbi Kang <jenabill@naver.com>
2022-07-23 00:52:32 +0900

    Refactor: linkMemo 관련 쿼리 키 수정

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -1,6 +1,7 @@
 export const categoriesKey = ["categories"] as const;
 
 export const linkMemoKeys = {
+  all: ["linkMemos"],
   filter: (categoryId: number, searchQuery: string) =>
     ["linkMemos", categoryId, searchQuery] as const,
   like: ["linkMemos", "like"] as const,
