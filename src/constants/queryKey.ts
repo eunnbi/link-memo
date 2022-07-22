b70aebe2a98b6d15631de14fea7caf59cd5b2ff7
Eunbi Kang <jenabill@naver.com>
2022-07-23 00:08:37 +0900

    Refactor: 모든 쿼리 키 값 뒤에 as const 붙임

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -1,10 +1,7 @@
-export const categoriesKey = ["categories"];
+export const categoriesKey = ["categories"] as const;
 
 export const linkMemoKeys = {
-  filter: (categoryId: number, searchQuery: string) => [
-    "linkMemos",
-    categoryId,
-    searchQuery,
-  ],
-  like: ["linkMemos", "like"],
+  filter: (categoryId: number, searchQuery: string) =>
+    ["linkMemos", categoryId, searchQuery] as const,
+  like: ["linkMemos", "like"] as const,
 };
