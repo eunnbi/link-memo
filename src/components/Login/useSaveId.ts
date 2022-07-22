ab8db026a1acc10b2da6895e667365a349069023
Eunbi Kang <jenabill@naver.com>
2022-07-22 21:43:17 +0900

    Refactor: useSaveId 훅의 파라미터 타입 변경 및 키 변경

--- src/components/Login/useSaveId.ts
+++ src/components/Login/useSaveId.ts
@@ -1,25 +1,28 @@
-import React, { useEffect } from 'react';
-import { useToggle } from '../../hooks/useToggle';
+import React, { useEffect } from "react";
+import { useToggle } from "../../hooks/useToggle";
+import { AuthState } from "../../types/auth";
+
+const ID = "id" as const;
 
 export const useSaveId = (
   id: string,
-  setForm: React.Dispatch<any>,
+  setForm: React.Dispatch<AuthState>
 ): [boolean, () => void] => {
   const [saveId, handleSaveId] = useToggle(false);
   useEffect(() => {
-    const value = localStorage.getItem('ID');
+    const value = localStorage.getItem("ID");
     if (value == null) return;
     handleSaveId();
     setForm({
       id: JSON.parse(value),
-      password: '',
+      password: "",
     });
   }, []);
   useEffect(() => {
     if (saveId) {
-      localStorage.setItem('ID', JSON.stringify(id));
+      localStorage.setItem(ID, JSON.stringify(id));
     } else {
-      localStorage.removeItem('ID');
+      localStorage.removeItem(ID);
     }
   }, [saveId, id]);
   return [saveId, handleSaveId];
