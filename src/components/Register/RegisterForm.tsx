00394c4ff62bce45b7af201e0a4a61f14e06946c
Eunbi Kang <jenabill@naver.com>
2022-07-22 22:34:03 +0900

    Refactor: RegisterForm 컴포넌트 로직을 구성하는 hook 수정

--- src/components/Register/RegisterForm.tsx
+++ src/components/Register/RegisterForm.tsx
@@ -2,35 +2,52 @@ import { useState } from "react";
 import FormInput from "../common/FormInput";
 import { Button } from "../common/styles/Button.styles";
 import { Form, FormRow, Text } from "./RegisterForm.styles";
-import { useRegister } from "./useRegister";
-import { useCheckPasswd } from "./useCheckPasswd";
+import { AuthState } from "../../types/auth";
 import { useForm } from "../../hooks/useForm";
+import { usePostRegister } from "./usePostRegister";
+import { useCheckPasswd } from "./useCheckPasswd";
+import { useCheckIdDuplicate } from "./useCheckIdDuplicate";
+import { useCheckFormValidation } from "./useCheckFormValidation";
 
 export interface IGuideText {
-  where: string;
+  where: "id" | "password" | "checkPasswd" | "";
   text: string;
   isWarning: boolean;
 }
 
 const RegisterForm = () => {
-  const [guideText, setGuideText] = useState({
+  const [guideText, setGuideText] = useState<IGuideText>({
     where: "",
     text: "",
     isWarning: true,
   });
-  const { form, onChange } = useForm({
+  const { form, onChange } = useForm<AuthState>({
     id: "",
-    passwd: "",
+    password: "",
     checkPasswd: "",
   });
-  const { id, passwd, checkPasswd } = form;
-  const { onRegister, checkIdDuplicate } = useRegister(
-    id,
-    passwd,
-    checkPasswd,
-    setGuideText
-  );
-  useCheckPasswd(passwd, checkPasswd, setGuideText);
+  const { id, password, checkPasswd } = form;
+  const { mutate } = usePostRegister(); // 회원가입 api 호출
+  const { checkIdDuplicate, duplicateCheck, IsDuplicate } =
+    useCheckIdDuplicate(setGuideText); // 아이디 중복 여부 체크 (api 호출)
+  const { checkFormValidation } = useCheckFormValidation(setGuideText); // 회원가입 폼 건증
+  useCheckPasswd(password, checkPasswd!, setGuideText); // 비밀번호 확인 입력란 유효성 검증
+
+  const onRegister = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      checkFormValidation(
+        id,
+        password,
+        checkPasswd!,
+        duplicateCheck.current,
+        IsDuplicate.current
+      )
+    ) {
+      mutate({ id, password });
+    }
+  };
+
   return (
     <Form onSubmit={onRegister}>
       <FormRow>
@@ -42,7 +59,7 @@ const RegisterForm = () => {
             password={false}
             onChange={onChange}
           />
-          <Button onClick={checkIdDuplicate} type="button">
+          <Button onClick={() => checkIdDuplicate(id)} type="button">
             중복확인
           </Button>
         </div>
@@ -52,14 +69,14 @@ const RegisterForm = () => {
       </FormRow>
       <FormRow>
         <FormInput
-          value={passwd}
-          name="passwd"
+          value={password}
+          name="password"
           placeholder="비밀번호"
           password={true}
           onChange={onChange}
         />
         <p>8~15자, 영문 및 숫자 반드시 포함</p>
-        {guideText.where === "passwd" && (
+        {guideText.where === "password" && (
           <Text warning={guideText.isWarning}>{guideText.text}</Text>
         )}
       </FormRow>
