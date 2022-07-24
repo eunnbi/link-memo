b205a2ae237364721063e839406401092e36b362
Eunbi Kang <jenabill@naver.com>
2022-07-24 14:17:07 +0900

    Design: p 태그에 띄어쓰기 반영되도록 스타일 수정

--- src/components/LinkMemo/LinkMemoItem/MemoContent.tsx
+++ src/components/LinkMemo/LinkMemoItem/MemoContent.tsx
@@ -25,12 +25,14 @@ const ContentBox = styled.div`
     flex-shrink: 0;
     margin-top: 0.5rem;
     font-size: 1.2rem;
+    color: ${({ theme }) => theme.color.textColor};
   }
 `;
 
 const Content = styled.p<{ show: boolean }>`
   font-size: 1rem;
   line-height: 2rem;
+  white-space: pre-line;
   ${({ show }) =>
     !show &&
     css`
