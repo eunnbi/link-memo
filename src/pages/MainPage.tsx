3766328cd5ef0d490afd01832ab072f619607072
Eunbi Kang <jenabill@naver.com>
2022-07-24 12:29:12 +0900

    Refactor: ThemeButton 컴포넌트 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,21 @@
 import CategoriesSection from "../components/Categories/CategoriesSection";
 import FavoritesSection from "../components/LinkMemo/Favorites/FavoritesSection";
-import MainHeader from "../components/Main/MainHeader";
+import Logout from "../components/Logout";
+import styled from "styled-components";
+import ThemeButton from "../components/ThemeButton";
 
-const MainPage = () => {
+interface MainPageProps {
+  changeTheme: () => void;
+}
+
+const MainPage = ({ changeTheme }: MainPageProps) => {
   return (
     <main>
-      <MainHeader />
+      <Heading>
+        <Logout />
+        <h1>Link Memo</h1>
+        <ThemeButton changeTheme={changeTheme} />
+      </Heading>
       <CategoriesSection />
       <FavoritesSection />
     </main>
@@ -13,3 +23,17 @@ const MainPage = () => {
 };
 
 export default MainPage;
+
+const Heading = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1.5rem 0 2rem;
+  h1 {
+    width: 100%;
+    font-family: Pangolin;
+    font-size: 2.5rem;
+    text-align: center;
+  }
+`;
