03c27d1b731ed4a18c8116d830e3fdd501c5b9b0
Eunbi Kang <jenabill@naver.com>
2022-07-24 12:26:05 +0900

    Add: GlobalStyle

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: ${({ theme }) => theme.color.bgColor};
+        color: ${({ theme }) => theme.color.textColor};
+        transition: all 0.3s ease-in-out;
+    }
+    @media ${({ theme }) => theme.device.tablet} {
+        html {
+            font-size: 15px;
+        }
+    }
+    @media ${({ theme }) => theme.device.mobile}, (max-height: 600px) {
+        html {
+            font-size: 13px;
+        }
+    }
+    @media ${({ theme }) => theme.device.smallMobile}, (max-height: 300px) {
+        html {
+            font-size: 10px;
+        }
+    }
+`;
