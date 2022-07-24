4a3da2a73ec4ab6f535f235a95327607769d5d95
Eunbi Kang <jenabill@naver.com>
2022-07-24 12:28:17 +0900

    Add: ThemeButton component

---
+++ src/components/ThemeButton/index.tsx
@@ -0,0 +1,22 @@
+import styled, { useTheme } from "styled-components";
+import { IoMoon, IoSunny } from "react-icons/io5";
+import { IconButton } from "../common/styles/IconButton.styles";
+
+interface ThemeButtonProps {
+  changeTheme: () => void;
+}
+
+const ThemeButton = ({ changeTheme }: ThemeButtonProps) => {
+  const { name } = useTheme();
+  return (
+    <IconButton onClick={changeTheme}>
+      {name === "light" ? <Sunny /> : <IoMoon />}
+    </IconButton>
+  );
+};
+
+export default ThemeButton;
+
+const Sunny = styled(IoSunny)`
+  color: #f29f21;
+`;
