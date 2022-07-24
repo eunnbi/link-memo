8c921ffa0527fe7721f630932d576d9bc2ac2df0
Eunbi Kang <jenabill@naver.com>
2022-07-25 02:39:11 +0900

    Refactor: LinkMemoList component

--- src/components/LinkMemo/LinkMemoList/index.tsx
+++ src/components/LinkMemo/LinkMemoList/index.tsx
@@ -1,13 +1,6 @@
 import styled from "styled-components";
-import { useState } from "react";
-import DeleteAlert from "./DeleteAlert";
-import ShareAlert from "./ShareAlert";
 import LinkMemoItem from "../LinkMemoItem";
-import {
-  LinkMemoId,
-  LinkMemoResponse,
-  LinkMemoShare,
-} from "../../../types/linkMemo";
+import { LinkMemoResponse } from "../../../types/linkMemo";
 
 interface LinkMemoListProps {
   linkMemos: LinkMemoResponse[] | undefined;
@@ -15,14 +8,6 @@ interface LinkMemoListProps {
 }
 
 const LinkMemoList = ({ linkMemos, noLinkMemos }: LinkMemoListProps) => {
-  const [deleteLinkMemo, setDeleteLinkMemo] = useState<LinkMemoId>({
-    memoId: 0,
-  });
-  const [shareLinkMemo, setShareLinkMemo] = useState<LinkMemoShare>({
-    linkName: "",
-    linkUrl: "",
-  });
-
   return linkMemos?.length === 0 ? (
     noLinkMemos ? (
       noLinkMemos
@@ -30,29 +15,11 @@ const LinkMemoList = ({ linkMemos, noLinkMemos }: LinkMemoListProps) => {
       <NoLinkMemos>아래 (+) 버튼을 눌러 링크 메모를 추가해보세요!</NoLinkMemos>
     )
   ) : (
-    <>
-      <List>
-        {linkMemos?.map((linkMemo) => (
-          <LinkMemoItem
-            key={linkMemo.memoId}
-            linkMemo={linkMemo}
-            clickRemoveMenu={() =>
-              setDeleteLinkMemo({
-                memoId: linkMemo.memoId!,
-              })
-            }
-            clickShareButton={() =>
-              setShareLinkMemo({
-                linkName: linkMemo.linkName,
-                linkUrl: linkMemo.linkUrl,
-              })
-            }
-          />
-        ))}
-      </List>
-      <DeleteAlert linkMemo={deleteLinkMemo} setLinkMemo={setDeleteLinkMemo} />
-      <ShareAlert linkMemo={shareLinkMemo} setLinkMemo={setShareLinkMemo} />
-    </>
+    <List>
+      {linkMemos?.map((linkMemo) => (
+        <LinkMemoItem key={linkMemo.memoId} linkMemo={linkMemo} />
+      ))}
+    </List>
   );
 };
 
