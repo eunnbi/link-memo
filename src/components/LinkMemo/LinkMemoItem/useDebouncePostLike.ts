8c357b918a90283805f2688f986a765d6826e29c
Eunbi Kang <jenabill@naver.com>
2022-07-24 14:37:37 +0900

    Refactor: useDebouncePostLike hook

--- src/components/LinkMemo/LinkMemoItem/useDebouncePostLike.ts
+++ src/components/LinkMemo/LinkMemoItem/useDebouncePostLike.ts
@@ -1,24 +1,12 @@
 import { useEffect, useRef } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { postLinkMemoLike } from "../../../api/linkMemos";
-import { SuccessResponse } from "../../../types";
-import { LinkMemoLikeRequest } from "../../../types/linkMemo";
-import { linkMemoKeys } from "../../../constants/queryKey";
+import { usePostLike } from "./usePostLike";
 
 export const useDebouncePostLike = (
   initialLike: boolean,
   like: boolean,
   memoId: number
 ) => {
-  const queryClient = useQueryClient();
-  const { mutate } = useMutation<SuccessResponse, unknown, LinkMemoLikeRequest>(
-    ({ value, memoId }) => postLinkMemoLike(value, memoId),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(linkMemoKeys.all);
-      },
-    }
-  );
+  const { mutate } = usePostLike();
 
   const timer = useRef<NodeJS.Timeout | null>(null);
   useEffect(() => {
