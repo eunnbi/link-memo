168a0ae83c4574128d01ab668fc5b9762257b919
Eunbi Kang <jenabill@naver.com>
2022-07-24 12:28:50 +0900

    Refactor: ThemeProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
 import LinkMemoPage from "./pages/LinkMemoPage";
 import EditPage from "./pages/EditPage";
+import { lightTheme, darkTheme } from "./styles/theme";
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+const THEME = "theme" as const;
 
 const queryClient = new QueryClient();
 function App() {
+  const [isLightTheme, setIsLightTheme] = useState(true);
+  const changeTheme = () => {
+    setIsLightTheme(!isLightTheme);
+    window.localStorage.setItem(THEME, JSON.stringify(!isLightTheme));
+  };
+
   useEffect(() => {
     console.log(process.env.REACT_APP_KAKAO_KEY);
     window.Kakao.init(process.env.REACT_APP_KAKAO_KEY);
     console.log(window.Kakao.isInitialized());
+
+    const theme = window.localStorage.getItem(THEME);
+    if (theme !== null) {
+      setIsLightTheme(JSON.parse(theme));
+    }
   }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/memo/*" element={<LinkMemoPage />}>
-            <Route path=":categoryId" element={<LinkMemoPage />} />
-          </Route>
-          <Route path="/edit/*" element={<EditPage />}>
-            <Route path=":memoId" element={<EditPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route
+              path="/main"
+              element={<MainPage changeTheme={changeTheme} />}
+            />
+            <Route path="/memo/*" element={<LinkMemoPage />}>
+              <Route path=":categoryId" element={<LinkMemoPage />} />
+            </Route>
+            <Route path="/edit/*" element={<EditPage />}>
+              <Route path=":memoId" element={<EditPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
