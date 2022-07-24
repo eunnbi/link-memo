38cc1bc4f5bd3407f96e600355d1b8de0a4d72d0
Eunbi Kang <jenabill@naver.com>
2022-07-24 15:04:34 +0900

    Refactor: Logout component

--- src/components/Logout/index.tsx
+++ src/components/Logout/index.tsx
@@ -1,9 +1,16 @@
 import { BiLogOut } from "react-icons/bi";
-import { useLogout } from "./useLogout";
+import { useNavigate } from "react-router-dom";
 import { IconButton } from "../common/styles/IconButton.styles";
+import { removeUserId } from "../../utils/auth";
 
 const Logout = () => {
-  const onLogout = useLogout();
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    removeUserId();
+    navigate("/");
+  };
+
   return (
     <IconButton onClick={onLogout}>
       <BiLogOut />
