163bd278e56c9a9b968aa0cae6b4120191e82f09
Eunbi Kang <jenabill@naver.com>
2022-07-24 15:41:09 +0900

    Design: SearchBar component

--- src/components/LinkMemo/SearchBar.tsx
+++ src/components/LinkMemo/SearchBar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { GoSearch } from "react-icons/go";
 import styled from "styled-components";
 
 interface SearchBarProps {
@@ -9,39 +8,25 @@ interface SearchBarProps {
 
 const SearchBar = ({ searchQuery, onChange }: SearchBarProps) => {
   return (
-    <Bar>
-      <Input
-        placeholder="검색"
-        onChange={onChange}
-        name="searchQuery"
-        value={searchQuery}
-      />
-      <GoSearch />
-    </Bar>
+    <Input
+      placeholder="🔎 검색"
+      onChange={onChange}
+      name="searchQuery"
+      value={searchQuery}
+    />
   );
 };
 
 export default SearchBar;
 
-const Bar = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  svg {
-    cursor: pointer;
-    color: gray;
-  }
-`;
-
 const Input = styled.input`
+  width: 100%;
   border-radius: 1rem;
   font-size: 1rem;
   padding: 0.5rem 1rem;
   flex-grow: 1;
   max-width: 400px;
-  background-color: ${({ theme }) => theme.name === "dark" && "#464646"};
-  border: ${({ theme }) => theme.name === "light" && "1px solid lightgray"};
+  background-color: ${({ theme }) => theme.color.grayBgColor};
   color: ${({ theme }) => theme.color.textColor};
   &::-moz-placeholder {
     color: ${({ theme }) => theme.name === "dark" && "lightgray"};
