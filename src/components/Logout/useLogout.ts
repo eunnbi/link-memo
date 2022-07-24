7ad128a15cc3a27bf243767c5ebcde5c63587a40
Eunbi Kang <jenabill@naver.com>
2022-07-24 11:44:40 +0900

    Rename: Logout

--- src/components/Logout/useLogout.ts
+++ src/components/Logout/useLogout.ts
@@ -1,11 +1,11 @@
-import { useNavigate } from 'react-router-dom';
-import { removeUserId } from '../../../utils/auth';
+import { useNavigate } from "react-router-dom";
+import { removeUserId } from "../../utils/auth";
 
 export const useLogout = () => {
   const navigate = useNavigate();
   const onLogout = () => {
     removeUserId();
-    navigate('/');
+    navigate("/");
   };
 
   return onLogout;
