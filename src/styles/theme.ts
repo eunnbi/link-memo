748c24ed956def4b7999c46b0fc54645005741c7
Eunbi Kang <jenabill@naver.com>
2022-07-24 14:13:08 +0900

    Refactor: theme 객체 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,9 +12,15 @@ const device = {
 
 export const lightTheme = {
   color: {
-    bgColor: "#fff",
+    bodyBgColor: "#fff",
     textColor: "#000",
-    shadowColor: "rgba(0, 0, 0, 0.15)",
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    borderColor: "#212121",
+    buttonColor: "#212121",
+    bgColor: "#fff",
+    red: "red",
+    blue: "blue",
+    hoverColor: "rgba(0, 0, 0, 0.05)",
   },
   device,
   name: "light",
@@ -22,9 +28,15 @@ export const lightTheme = {
 
 export const darkTheme = {
   color: {
-    bgColor: "#000",
+    bodyBgColor: "#252525",
     textColor: "#fff",
     shadowColor: "#000",
+    borderColor: "#fff",
+    buttonColor: "#101010",
+    bgColor: "#000",
+    red: "#F07470",
+    blue: "#56A9FF",
+    hoverColor: "#363636",
   },
   device,
   name: "dark",
