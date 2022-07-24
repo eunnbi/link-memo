fb45ebb48c17d9a6eac85578fe6c457dda3d3e3e
Eunbi Kang <jenabill@naver.com>
2022-07-25 02:31:19 +0900

    Add: CategoryAddButton

---
+++ src/components/Categories/CategoryAddButton.tsx
@@ -0,0 +1,14 @@
+import { useModal } from "../../hooks/useModal";
+import PlusButton from "../common/PlusButton";
+import AddModal from "./AddModal";
+
+const CategoryAddButton = () => {
+  const { openModal } = useModal();
+  const onClick = () => {
+    openModal(AddModal);
+  };
+
+  return <PlusButton onClick={onClick} />;
+};
+
+export default CategoryAddButton;
