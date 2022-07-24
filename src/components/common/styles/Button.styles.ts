60533181ffe5ba3c02bb3b3586b7c7f2358816d2
Eunbi Kang <jenabill@naver.com>
2022-07-24 15:57:18 +0900

    Design: Button component

--- src/components/common/styles/Button.styles.ts
+++ src/components/common/styles/Button.styles.ts
@@ -2,12 +2,11 @@ import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const buttonStyle = css`
-  background-color: ${({ theme }) => theme.color.buttonColor};
-  border-radius: 10px;
+  background-color: #101010;
+  border-radius: 0.4rem;
   color: #fff;
   font-size: 1rem;
   padding: 0.5rem 1rem;
-  box-shadow: ${({ theme }) => `1px 5px 10px ${theme.color.shadowColor}`};
   text-align: center;
 `;
 
