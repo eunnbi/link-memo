e67a21e5e3d95f9da4f451cd8e5fcc93576bc63a
Eunbi Kang <jenabill@naver.com>
2022-07-24 12:28:32 +0900

    Refactor: index.css import 경로 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./modules";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App";
 
 const store = createStore(rootReducer);
