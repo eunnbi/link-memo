f43cf978ec0ce8592809f7663a8f72c0024b32db
Eunbi Kang <jenabill@naver.com>
2022-07-25 02:35:07 +0900

    Rename: LinkMemoItem 폴더 아래 hooks 폴더 추가

--- src/components/LinkMemo/LinkMemoItem/hooks/useDeleteLinkMemo.ts
+++ src/components/LinkMemo/LinkMemoItem/hooks/useDeleteLinkMemo.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteLinkMemo } from "../../../api/linkMemos";
-import { SuccessResponse } from "../../../types";
-import { LinkMemoId } from "../../../types/linkMemo";
+import { deleteLinkMemo } from "../../../../api/linkMemos";
+import { SuccessResponse } from "../../../../types";
+import { LinkMemoId } from "../../../../types/linkMemo";
 
 export const useDeleteLinkMemo = (onClose: () => void) => {
   const queryClient = useQueryClient();
--- src/components/LinkMemo/LinkMemoItem/hooks/usePostLike.ts
+++ src/components/LinkMemo/LinkMemoItem/hooks/usePostLike.ts
@@ -1,8 +1,8 @@
 import { useQueryClient, useMutation } from "@tanstack/react-query";
-import { postLinkMemoLike } from "../../../api/linkMemos";
-import { SuccessResponse } from "../../../types";
-import { LinkMemoLikeRequest } from "../../../types/linkMemo";
-import { linkMemoKeys } from "../../../constants/queryKey";
+import { postLinkMemoLike } from "../../../../api/linkMemos";
+import { SuccessResponse } from "../../../../types";
+import { LinkMemoLikeRequest } from "../../../../types/linkMemo";
+import { linkMemoKeys } from "../../../../constants/queryKey";
 
 export const usePostLike = () => {
   const queryClient = useQueryClient();
