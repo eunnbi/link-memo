f0e88209891a2f849b89d6459e1d41870a2c3b21
Eunbi Kang <jenabill@naver.com>
2022-07-26 01:29:59 +0900

    Refactor: SaveIdCheckBox component

--- src/components/Login/SaveIdCheckBox.tsx
+++ src/components/Login/SaveIdCheckBox.tsx
@@ -1,15 +1,36 @@
-import { AuthState } from "../../types/auth";
+import { useEffect } from "react";
 import CheckBox from "../common/CheckBox";
-import { useSaveId } from "./hooks/useSaveId";
+import { useToggle } from "../../hooks/useToggle";
+import { useChangeInput } from "./hooks/useChangeInput";
+import { AuthState } from "../../types/auth";
 
 interface SaveIdCheckBoxProps {
   id: AuthState["id"];
 }
 
+const ID = "id" as const;
+
 const SaveIdCheckBox = ({ id }: SaveIdCheckBoxProps) => {
-  const [saveId, handleSaveId] = useSaveId(id);
+  const [saveId, onToggle] = useToggle(false);
+  const { changeInputValue } = useChangeInput();
+
+  useEffect(() => {
+    const value = localStorage.getItem("ID");
+    if (value == null) return;
+    onToggle();
+    changeInputValue("id", JSON.parse(value));
+  }, []);
+
+  useEffect(() => {
+    if (saveId) {
+      localStorage.setItem(ID, JSON.stringify(id));
+    } else {
+      localStorage.removeItem(ID);
+    }
+  }, [saveId, id]);
+
   return (
-    <CheckBox label="아이디 저장" isChecked={saveId} onToggle={handleSaveId} />
+    <CheckBox label="아이디 저장" isChecked={saveId} onToggle={onToggle} />
   );
 };
 
