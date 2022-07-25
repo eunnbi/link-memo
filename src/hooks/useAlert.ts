802e0ca0ba3231db00b323ceb4aa83537fd68fff
Eunbi Kang <jenabill@naver.com>
2022-07-25 15:46:39 +0900

    Remove: useAlert hook

--- src/hooks/useAlert.ts
+++
@@ -1,8 +0,0 @@
-import { useCallback, useState } from "react";
-
-export const useAlert = () => {
-  const [show, setShow] = useState(false);
-  const onAlert = useCallback(() => setShow(true), []);
-  const onClose = useCallback(() => setShow(false), []);
-  return { show, onAlert, onClose };
-};
