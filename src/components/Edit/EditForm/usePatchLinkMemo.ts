3cc9fd66ac693b1502aea24c19f27257a5d6fac5
Eunbi Kang <jenabill@naver.com>
2022-07-26 01:33:48 +0900

    Refactor: useMutation variable 타입 수정

--- src/components/Edit/EditForm/usePatchLinkMemo.ts
+++ src/components/Edit/EditForm/usePatchLinkMemo.ts
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { patchLinkMemo } from "../../../api/linkMemos";
 import { SuccessResponse } from "../../../types";
-import { LinkMemoRequest } from "../../../types/linkMemo";
+import { LinkMemoPatchRequest } from "../../../types/linkMemo";
 import { linkMemoKeys } from "../../../constants/queryKey";
 
 export const usePatchLinkMemo = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  return useMutation<SuccessResponse, unknown, LinkMemoRequest>(
+  return useMutation<SuccessResponse, unknown, LinkMemoPatchRequest>(
     ({ linkName, linkUrl, content, categoryId, memoId }) =>
       patchLinkMemo(linkName, linkUrl, content, categoryId, memoId!),
     {
