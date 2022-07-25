ede9de3172d53fd561c679c4d8ed69e3151e4572
Eunbi Kang <jenabill@naver.com>
2022-07-26 01:32:30 +0900

    Refactor: linkMemo 관련 타입 수정

--- src/types/linkMemo.ts
+++ src/types/linkMemo.ts
@@ -11,25 +11,23 @@ export interface LinkMemoId {
   memoId: number;
 }
 
-export interface LinkMemoResponse {
-  memoId?: LinkMemoId["memoId"];
-  linkName: LinkMemoState["linkName"];
-  linkUrl: LinkMemoState["linkUrl"];
-  content: LinkMemoState["content"];
-  categoryId: Category["categoryId"];
-  categoryName?: Category["categoryName"];
-  like?: boolean;
-}
+export type LinkMemoResponse = LinkMemoId &
+  Category &
+  Pick<LinkMemoState, "linkName" | "linkUrl" | "content"> & { like: boolean };
 
 export interface LinkMemosGetResponse {
   linkMemos: LinkMemoResponse[];
   message?: string;
 }
 
-export type LinkMemoRequest = LinkMemoResponse;
+export type LinkMemoPostRequest = Pick<
+  LinkMemoResponse,
+  "linkName" | "linkUrl" | "content" | "categoryId" | "categoryName"
+>;
 
-export interface LinkMemoLikeRequest {
-  memoId: LinkMemoId["memoId"];
+export type LinkMemoPatchRequest = LinkMemoPostRequest & LinkMemoId;
+
+export interface LinkMemoLikeRequest extends LinkMemoId {
   value: boolean;
 }
 
