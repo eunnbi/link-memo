467e3aedbb53288ca41c14366861ec3de7a5a1f6
Eunbi Kang <jenabill@naver.com>
2022-07-25 15:18:59 +0900

    Design: FormInput 컴포넌트에 theme style 적용

--- src/components/common/FormInput/FormInput.styles.ts
+++ src/components/common/FormInput/FormInput.styles.ts
@@ -11,12 +11,23 @@ export const Label = styled.label`
 
 export const Input = styled.input`
   flex-grow: 1;
-  border-bottom: 1px solid #000;
+  border-bottom: ${({ theme }) => `1px solid ${theme.color.borderColor}`};
   padding: 0.5rem 0.8rem;
   font-size: 1.2rem;
-  border-radius: ${({ theme }) => (theme.name === "light" ? "0" : "0.8rem")};
+  border-radius: 0;
+  background-color: transparent;
   outline: none;
   font-family: NotoSans;
+  color: ${({ theme }) => theme.color.textColor};
+  &::placeholder {
+    color: ${({ theme }) => theme.color.placeholderColor};
+  }
+  &::-moz-placeholder {
+    color: ${({ theme }) => theme.color.placeholderColor};
+  }
+  &::-webkit-input-placeholder {
+    color: ${({ theme }) => theme.color.placeholderColor};
+  }
 `;
 
 export const InputWrapper = styled.div`
