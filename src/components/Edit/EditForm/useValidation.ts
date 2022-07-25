357612e64a36ca471118155536fbd6d04b0b217c
Eunbi Kang <jenabill@naver.com>
2022-07-25 15:18:30 +0900

    Add: useValidation hook for EditForm

---
+++ src/components/Edit/EditForm/useValidation.ts
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { setWarningText } from "../../../modules/login";
+import { LinkMemoState } from "../../../types/linkMemo";
+
+type ValidateState = Pick<LinkMemoState, "linkName" | "linkUrl">;
+
+export const useValidation = () => {
+  const dispatch = useDispatch();
+  const validateForm = ({ linkName, linkUrl }: ValidateState): boolean => {
+    if (linkName === "") {
+      dispatch(setWarningText("링크 이름은 필수 입력 항목입니다."));
+      return false;
+    }
+    if (linkUrl === "") {
+      dispatch(setWarningText("링크 주소는 필수 입력 항목입니다."));
+      return false;
+    }
+    return true;
+  };
+  return { validateForm };
+};
