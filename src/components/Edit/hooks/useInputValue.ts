5c8d34f110c20bf209aed0d41f19c00ecc344a69
Eunbi Kang <jenabill@naver.com>
2022-07-25 15:30:20 +0900

    Remove: useInputValue hook

--- src/components/Edit/hooks/useInputValue.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../../modules";
-
-export const useInputValue = () => {
-  return useSelector((rootState: RootState) => rootState.linkMemo);
-};
