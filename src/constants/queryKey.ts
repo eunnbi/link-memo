5aaab71c86e48891acf4791fd7d4de8a5ac585f4
Eunbi Kang <jenabill@naver.com>
2022-07-25 15:55:00 +0900

    Refactor: 모든 queryKey에 as const 추가

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -1,7 +1,7 @@
 export const categoriesKey = ["categories"] as const;
 
 export const linkMemoKeys = {
-  all: ["linkMemos"],
+  all: ["linkMemos"] as const,
   filter: (categoryId: number, searchQuery: string) =>
     ["linkMemos", categoryId, searchQuery] as const,
   like: ["linkMemos", "like"] as const,
