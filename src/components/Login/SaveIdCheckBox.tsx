8f984c848976c17402641ea5712274e2a7f7d6b3
Eunbi Kang <jenabill@naver.com>
2022-07-25 15:32:15 +0900

    Refactor: SaveIdCheckBox component

--- src/components/Login/SaveIdCheckBox.tsx
+++ src/components/Login/SaveIdCheckBox.tsx
@@ -1,14 +1,13 @@
 import { AuthState } from "../../types/auth";
 import CheckBox from "../common/CheckBox";
-import { useSaveId } from "./useSaveId";
+import { useSaveId } from "./hooks/useSaveId";
 
 interface SaveIdCheckBoxProps {
   id: AuthState["id"];
-  setForm: React.Dispatch<AuthState>;
 }
 
-const SaveIdCheckBox = ({ id, setForm }: SaveIdCheckBoxProps) => {
-  const [saveId, handleSaveId] = useSaveId(id, setForm);
+const SaveIdCheckBox = ({ id }: SaveIdCheckBoxProps) => {
+  const [saveId, handleSaveId] = useSaveId(id);
   return (
     <CheckBox label="아이디 저장" isChecked={saveId} onToggle={handleSaveId} />
   );
