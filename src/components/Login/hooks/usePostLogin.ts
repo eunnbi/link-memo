39acfc574b2cb330a236f7f8f5ec760f05d57f91
Eunbi Kang <jenabill@naver.com>
2022-07-26 17:18:41 +0900

    Refactor: usePostLogin hook

--- src/components/Login/hooks/usePostLogin.ts
+++ src/components/Login/hooks/usePostLogin.ts
@@ -21,7 +21,6 @@ export const usePostLogin = () => {
         const { success, userId } = data;
         if (success) {
           storeUserId(userId!);
-          changeWarningText("");
           navigate("/main");
         } else {
           changeWarningText(
