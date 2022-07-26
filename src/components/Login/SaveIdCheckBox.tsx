466c1d0172ba89c389bb963c7006394474bac01b
Eunbi Kang <jenabill@naver.com>
2022-07-26 17:21:02 +0900

    Refactor: SaveIdCheckBox 컴포넌트 로직을 useSaveId 커스텀 훅으로 분리

--- src/components/Login/SaveIdCheckBox.tsx
+++ src/components/Login/SaveIdCheckBox.tsx
@@ -1,33 +1,8 @@
-import { useEffect } from "react";
 import CheckBox from "../common/CheckBox";
-import { useToggle } from "../../hooks/useToggle";
-import { useChangeInput } from "./hooks/useChangeInput";
-import { AuthState } from "../../types/auth";
+import { useSaveId } from "./hooks/useSaveId";
 
-interface SaveIdCheckBoxProps {
-  id: AuthState["id"];
-}
-
-const ID = "id" as const;
-
-const SaveIdCheckBox = ({ id }: SaveIdCheckBoxProps) => {
-  const [saveId, onToggle] = useToggle(false);
-  const { changeInputValue } = useChangeInput();
-
-  useEffect(() => {
-    const value = localStorage.getItem("ID");
-    if (value == null) return;
-    onToggle();
-    changeInputValue("id", JSON.parse(value));
-  }, []);
-
-  useEffect(() => {
-    if (saveId) {
-      localStorage.setItem(ID, JSON.stringify(id));
-    } else {
-      localStorage.removeItem(ID);
-    }
-  }, [saveId, id]);
+const SaveIdCheckBox = () => {
+  const { saveId, onToggle } = useSaveId();
 
   return (
     <CheckBox label="아이디 저장" isChecked={saveId} onToggle={onToggle} />
