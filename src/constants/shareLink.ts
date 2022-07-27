283c20578ad4c9b8742c0f941d3e20f0d104c847
Eunbi Kang <jenabill@naver.com>
2022-07-27 18:38:43 +0900

    Refactor: 공유 링크 수정

--- src/constants/shareLink.ts
+++ src/constants/shareLink.ts
@@ -1,2 +1,2 @@
 export const shareLinkParam = "linkUrl" as const;
-export const shareLink = `https://link-memo.netlify.app/share?${shareLinkParam}=`;
+export const shareLink = `https://link-memo.netlify.app?${shareLinkParam}=`;
